6370377b9d31cb895af204fe027667d320d6ae5f
Valentino1746 <valentinomatamoros2@gmail.com>
2026-05-07 00:15:59 -0600

    feat(sp-08):creation of supplier and category components

--- src/app/modules/home/supplier/supplier.spec.ts
+++ src/app/modules/home/supplier/supplier.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { Catalog } from './catalog';
+import { Supplier } from './supplier';
 
-describe('Catalog', () => {
-  let component: Catalog;
-  let fixture: ComponentFixture<Catalog>;
+describe('Supplier', () => {
+  let component: Supplier;
+  let fixture: ComponentFixture<Supplier>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Catalog]
+      imports: [Supplier]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(Catalog);
+    fixture = TestBed.createComponent(Supplier);
     component = fixture.componentInstance;
     await fixture.whenStable();
   });
